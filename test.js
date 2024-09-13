48b87c8643af479cb95f908af512c90eaf821949
Vania Natalie <vanianatalie02@gmail.com>
2024-09-13 11:45:28 +0700

    first commit

---
+++ test.js
@@ -0,0 +1,13 @@
+console.log("Hello, world!");
+
+
+console.log(123);
+
+
+console.log("The sum of 5 + 3 is:", 5 + 3);
+
+
+console.log({ name: "Alice", age: 25 });
+
+
+console.log([1, 2, 3, 4, 5]);
